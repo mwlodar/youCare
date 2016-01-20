61b9a08faa0e77dccccf7e4f75b19041e969b600
Matt Wlodarski <mattwlodarski@Matts-Mac-mini.local>
2016-01-19 22:32:50 -0500

    seventh commit - completed lumbar assessment views

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,45 +70,81 @@ angular.module('app.controllers', [])
 
 }]) //End lumbarController section
 
-.controller('lumbarAssessmentController', ['$scope', '$http', function($scope, $http) {
+.controller('lumbarAssessmentController', ['$scope', '$http', '$ionicScrollDelegate', 
+  function($scope, $http, $ionicScrollDelegate) {
 
-  $http.get('js/centCB.json').success(function (data){
-      $scope.items = data;
-
-    });
-
+  $scope.activeSection = 0;
+  $scope.activeCycle = 1;
+  $scope.choice = {
+      mechChoice: "",
+      sympChoice: ""
+    };
   $scope.centValues = [0,0,0,0,0,0];
-  $scope.sum = 0;
+  $scope.postCentValues = [0,0,0,0,0,0];
+  $scope.preCentSum = 0;
+  $scope.postCentSum = 0;
   $scope.isChecked = {
-       value : false
+       valueOne : false,
+       valueTwo: false
      };
 
 
   $scope.uncheckAll = function() {
     $scope.centValues = [];
-  $scope.centValues = [0,0,0,0,0,0];
+    $scope.centValues = [0,0,0,0,0,0];
 
   };
 
   $scope.uncheckNone = function() {
-    $scope.isChecked.value = false;
+    $scope.isChecked.valueOne = false;
 
   };
 
-
-
   $scope.preCentralization = function() {
     
-    if ($scope.sum > 0) {
-      $scope.sum = 0;
+    if ($scope.preCentSum > 0) {
+      $scope.preCentSum = 0;
     }
    
     for (var i = $scope.centValues.length - 1; i >= 0; i--) {
-      $scope.sum += $scope.centValues[i];
+      $scope.preCentSum += $scope.centValues[i];
     }
     // alert($scope.sum);
-    return $scope.sum; 
+    return $scope.preCentSum; 
 
+    };
+
+  $scope.uncheckAllTwo = function() {
+    $scope.postCentValues = [];
+    $scope.postCentValues = [0,0,0,0,0,0];
+
+  };
+
+  $scope.uncheckNoneTwo = function() {
+    $scope.isChecked.valueTwo = false;
+
+  };
+
+  $scope.postCentralization = function() {
+    
+    if ($scope.postCentSum > 0) {
+      $scope.postCentSum = 0;
+    }
+   
+    for (var i = $scope.postCentValues.length - 1; i >= 0; i--) {
+      $scope.postCentSum += $scope.postCentValues[i];
+    }
+    // alert($scope.sum);
+    return $scope.postCentSum; 
+
+    };
+
+    $scope.nextView = function() {
+      return $scope.activeSection += 1;
+    }
+
+     $scope.scrollTop = function() { 
+      $ionicScrollDelegate.scrollTop();
     };
 
     
